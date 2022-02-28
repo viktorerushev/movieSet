defad3c10c0a671b920c3e12b9c0c0eadbe04d6b
viktorerushev <viktorerushev@gmail.com>
2022-02-28 18:51:42 +0100

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn movieSet.wsgi --log-file -
--- movieSet/settings.py
+++ movieSet/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fe$+b4573sp!s36e1d26rqz*j+7kv4ao5v)8l#lujai(qe5txq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['movieset.herokuapp.com', '127.0.0.1']
 
 
 # Application definition

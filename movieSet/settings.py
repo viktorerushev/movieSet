fc9946d8b9520ad7d4c9df85d3bd7f2ae3fd783f
viktorerushev <viktorerushev@gmail.com>
2022-02-28 19:17:55 +0100

    Revert "Initial commit"
    This reverts commit defad3c10c0a671b920c3e12b9c0c0eadbe04d6b.

--- movieSet/settings.py
+++ movieSet/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fe$+b4573sp!s36e1d26rqz*j+7kv4ao5v)8l#lujai(qe5txq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['movieset.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
